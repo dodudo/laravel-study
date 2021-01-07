6e0504cad0fd69db562c393f6068f8a612f1014d
dodudo <dudamaode@gmail.com>
2021-01-07 16:27:19 +0800

    请求和依赖注入

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
+use Barryvdh\Debugbar\Facade as DebugBar;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
-    public function index(){
-        return \request()->all();
+    public function index(Request $request,$id,$uid){
+        DebugBar::info($id.'::::::'.$uid);
+        //使用 path()方法， 可以得到当前的 uri 路径
+        DebugBar::info($request->path());
+        //使用 is()方法， 可以判断当前的 uri 是否匹配；
+        DebugBar::info($request->is('user/*'));
+        //使用 url()和 fullUrl()得到 url 地址， 带参数和不带参数区别,?后面参数不一样
+        DebugBar::info($request->url());
+        DebugBar::info($request->fullUrl());
+        //使用 isMethod()来判断 HTTP 请求的方式， get、 post 等等；
+        DebugBar::info($request->isMethod('GET'));
+        return view('user');
+//        return \request()->all();
     }
 }
