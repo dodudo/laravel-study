0eba41d147063bd4f49236146c6700aa51763e68
dodudo <dudamaode@gmail.com>
2021-01-08 11:43:57 +0800

    session

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Barryvdh\Debugbar\Facade as DebugBar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
@@ -77,7 +78,52 @@ class UserController extends Controller
 //        $cookie = \cookie('gender', '男', 10);
 //        Cookie::queue($cookie);
 //        return response('dsa')->cookie('name');
-        return Cookie::get('gender');
+//        return Cookie::get('gender');
+        //session
+//        DebugBar::info($request->session()->all());
+        //获取一个session,闭包设置默认值
+//        DebugBar::info($request->session()->get('_token',function (){
+//            return "没有这个session";
+//        }));
+        //通过Session获取
+//        DebugBar::info(Session::get('_token'));
+        //助手函数，参数2还可以设置默认值
+//        $session = \session('name', '没有获取到这个session');
+//        DebugBar::info($session);
+        //判断是否存在
+//        DebugBar::info(\session()->has('_token'));
+//        DebugBar::info(Session::has("_token"));
+//        DebugBar::info($request->session()->has('_token'));
+//        DebugBar::info(\session()->exists('_token'));
+        //也可以使用助手函数传递数组的方式或 put()方法， 来存储 session 值
+//        \session(['name'=>'mr.do']);
+//        Session::put('name','ms.zhou');
+        //使用 push()方法， 可以存储数组， 支持 request()方式；
+//        Session::push('info.name','mr.do');
+//        Session::push('info.name','mr.er');
+//        Session::push('info.name','mr.z');
+//使用 flash()方法， 获取后自动删除， 支持 request()方式；
+//        Session::flash('name','mrs');
+//        DebugBar::info(\session('name'));
+        //如果使用闪存数据， 本次请求不要立刻自行删除， 可以使用 reflash()；
+        //本次请求获取，不要删除数据，给下一次请求时再自行删除，这是保存所有闪存数据
+//        Session::reflash();
+//        Session::keep(['name']); //保存单独的删除数据
+//        forget()可以删除一条或多条 session 数据， 支持 request()方式；
+        //        \session()->forget('info');
+//        \session(['info'=>'gsad']);
+
+        //删除一条数据，并返回
+//        DebugBar::info( Session::pull('info'));
+
+        //删除所有数据
+//        Session::flush();
+
+//        DebugBar::info(\session()->all());
+        //regenerate生成id
+        Session::regenerate();
+        DebugBar::info(Cookie::get('laravel_session'));
+        return view('user');
     }
     public function userForm(){
         return view('user_form');
